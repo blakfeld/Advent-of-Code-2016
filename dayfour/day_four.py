8183c7f51a8982c5ce18be57a2180bdec9888fee
Corwin Brown <blakfeld@Nightwing.local>
2016-12-30 20:13:19 -0600

    Part two of Day 4

--- dayfour/day_four.py
+++ dayfour/day_four.py
@@ -15,15 +15,29 @@ totally-real-room-200[decoy] is not.
 Of the real rooms from the list above, the sum of their sector IDs is 1514.
 
 What is the sum of the sector IDs of the real rooms?
+
+--- Part Two ---
+
+With all the decoy data out of the way, it's time to decrypt this list and get moving.
+
+The room names are encrypted by a state-of-the-art shift cipher, which is nearly unbreakable without the right software. However, the information kiosk designers at Easter Bunny HQ were not expecting to deal with a master cryptographer like yourself.
+
+To decrypt a room name, rotate each letter forward through the alphabet a number of times equal to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on. Dashes become spaces.
+
+For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
+
+What is the sector ID of the room where North Pole objects are stored?
 """
 from __future__ import print_function
 
 import re
 import sys
+import string
 from collections import Counter
 
 
 INPUT = 'input.txt'
+NORTH_POLE_NAME='northpole object storage'
 
 
 def read_input(fpath):
@@ -52,8 +66,8 @@ def format_sector_id(encrypted_room):
         tuple.
     """
     checksum = re.search(r'\[[a-z]+\]', encrypted_room).group(0)
-    room_name = filter(lambda s: s.isalpha(),
-                       encrypted_room.replace(checksum, ''))
+    room_name = filter(lambda s: s.isalpha() or s == '-',
+                       encrypted_room.replace(checksum, '')).rstrip('-')
     sector_id = int(filter(lambda d: d.isdigit(), encrypted_room))
     checksum = re.sub(r'(\[|\])', '', checksum)
 
@@ -80,33 +94,96 @@ def generate_checksum(room_name):
     return checksum
 
 
-def sum_valid_sectors(encrypted_ids):
+def get_valid_rooms(encrypted_rooms):
     """
-    Determine if a sector is valid, then sum it's id.
+    Determine if a room is valid, then sum it's id.
 
     Args:
-        encrypted_ids (list):   The list of IDs to check.
+        encrypted_rooms (list):   The list of IDs to check.
+
+    Returns:
+        list
+    """
+    valid_rooms = []
+    for encrypted_room in encrypted_rooms:
+        room_name, sector_id, checksum = encrypted_room
+        room_name = room_name.replace('-', '')
+        checksum = ''.join(sorted(checksum))
+        if checksum == generate_checksum(room_name):
+            valid_rooms.append(encrypted_room)
+
+    return valid_rooms
+
+
+def decode_room_name(room_name, sector_id):
+    """
+    Using a shift cipher, decode the room name.
+
+    Args:
+        room_name (str):    The encoded room name.
+        sector_id (int):    The sector ID to use with the shift
+                                cipher.
+
+    Returns:
+        str
+    """
+    decoded_name = []
+    alpha = string.ascii_lowercase
+    #print(room_name)
+    for c in room_name:
+        if c == '-':
+            decoded_name.append(' ')
+            continue
+
+        i = alpha.index(c)
+        decoded_name.append(alpha[(i + sector_id) % len(alpha)])
+
+    return ''.join(decoded_name)
+
+
+def find_north_pole(valid_rooms):
+    """
+    Decode the room names and find the north pole.
+
+    Args:
+        valid_rooms (list):     A list of valid rooms to decode/search.
+
+    Returns:
+        tuple
+    """
+    for room in valid_rooms:
+        room_name, sector_id, checksum = room
+        decoded_name = decode_room_name(room_name, sector_id)
+        if decoded_name == NORTH_POLE_NAME:
+            return decoded_name, sector_id
+
+
+
+def sum_valid_rooms(valid_rooms):
+    """
+    Get the sum of all the sector IDs in valid rooms.
+
+    Args:
+        valid_rooms (list):     List containting tuples of valid rooms.
 
     Returns:
         int
     """
-    count = 0
-    for encrypted_id in encrypted_ids:
-        room_name, sector_id, checksum = encrypted_id
-        checksum = ''.join(sorted(checksum))
-        if checksum == generate_checksum(room_name):
-            count += sector_id
-
-    return count
+    # Valid Room:
+    #   (room_name, sector_id, checksum)
+    return sum([i[1] for i in valid_rooms])
 
 
 def main():
     """
     Main.
     """
-    encrypted_ids = read_input(INPUT)
+    encrypted_rooms = read_input(INPUT)
+    valid_rooms = get_valid_rooms(encrypted_rooms)
 
-    print('Part One: {}'.format(sum_valid_sectors(encrypted_ids)))
+    print('Part One: {}'.format(sum_valid_rooms(valid_rooms)))
+
+    print('Part Two: {}'.format(find_north_pole(valid_rooms)))
 
 
 if __name__ == '__main__':
